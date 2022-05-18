359b594fea8009b6ba3ffdb9554f82d634263490
Alexandre Burlot <alexandre.burlot@cpe.fr>
2022-05-18 16:20:29 +0200

    possibilité de créer des cartes

--- Atelier2/src/main/java/com/sp/model/Card.java
+++ Atelier2/src/main/java/com/sp/model/Card.java
@@ -54,11 +54,11 @@ public class Card {
 		this.attack = attack;
 	}
 
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
