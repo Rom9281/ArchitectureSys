d3d652cba9710717942ac744c46110cf67cf7170
Rom9281 <34047259+Rom9281@users.noreply.github.com>
2022-05-23 09:06:48 +0200

    Changement mineurs sur le microuser

--- Atelier3/microuser/src/main/java/com/sp/rest/UserRestCrt.java
+++ Atelier3/microuser/src/main/java/com/sp/rest/UserRestCrt.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sp.model.User;
@@ -22,13 +23,23 @@ public class UserRestCrt {
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/user")
 	public Iterable<User> getAllUser() {
-		Iterable<User> c = uService.getAllUser();
-		return c;
+		Iterable<User> ulist = uService.getAllUser();
+		return ulist;
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/user/{id}")
 	public User getUser(@PathVariable String id) {
-		User c = uService.getUser(Integer.valueOf(id));
-		return c;
+		User u = uService.getUser(Integer.valueOf(id));
+		return u;
+	}
+	/**
+	 * Requete pour generer les cartes lors de la création d'un utilisateur
+	 * @PARAM User user
+	 * @RETURN Integer Id
+	 * */
+	@RequestMapping(method = RequestMethod.GET, value = "/createUser")
+	public User getUser(@RequestParam Integer id) {
+		User u = uService.getUser(Integer.valueOf(id));
+		return u;
 	}
 }
